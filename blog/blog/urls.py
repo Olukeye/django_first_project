8350211628f5f96c7207925268aac442c24ac89e
Olukeye <seunolukeye2000@gmail.com>
2022-12-14 11:45:25 +0100

    implemented profile api

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static 
 from users import views as user_views
 
 
@@ -12,3 +14,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), 
     path('', include('blogger.urls')),
 ]
+
+# concantinating media urlpatterns to the urlpattherns to display profile image
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
