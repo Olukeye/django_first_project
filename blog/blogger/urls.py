6d77f16c7028eb0a1e1931e52d4003d1da671e61
Olukeye <seunolukeye2000@gmail.com>
2022-12-16 13:47:43 +0100

    implemented single-post view and url

--- blog/blogger/urls.py
+++ blog/blogger/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.PostListView.as_view(), name='home'),     #rather than use the usual convention "path('', views.home, name='home')"
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('posts/<str:pk>', views.singleblog, name='posts')
+    # path('posts/<str:pk>', views.singleblog, name='posts')
 ]
