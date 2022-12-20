48cc4059b949ae018141e77c997b0b326bd86b35
Olukeye <seunolukeye2000@gmail.com>
2022-12-20 13:47:45 +0100

    implemented create post form api

--- blog/blogger/urls.py
+++ blog/blogger/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 from . import views
 
 
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name='home'),     #rather than use the usual convention "path('', views.home, name='home')"
-    path('post/<str:pk>', views.BlogDetailView.as_view(), name='blogger-detail'),  
+    path('', BlogListView.as_view(), name='home'),     #rather than use the usual convention "path('', views.home, name='home')"
+    path('post/<str:pk>', BlogDetailView.as_view(), name='blogger-detail'), 
+    path('post/new/', BlogCreateView.as_view(), name='blogger-create'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     # path('posts/<str:pk>', views.singleblog, name='posts')
--- blog/blogger/views.py
+++ blog/blogger/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView, DetailView, CreateView )
 from .models import Blog
 
 
@@ -20,6 +20,14 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Blog
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
