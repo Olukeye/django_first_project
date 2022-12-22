ba9ab637f17278562774438e4bb9ca996a3d98db
Olukeye <seunolukeye2000@gmail.com>
2022-12-22 18:27:15 +0100

    implemented get_all_user_post

--- blog/blogger/urls.py
+++ blog/blogger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, UserBlogListView
 from . import views
 
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),     #rather than use the usual convention "path('', views.home, name='home')"
+    path('user/<str:username>', UserBlogListView.as_view(), name='user-blogs'),
     path('post/<str:pk>', BlogDetailView.as_view(), name='blogger-detail'),
     path('post/<str:pk>/update', BlogUpdateView.as_view(), name='blogger-update'), 
     path('post/<str:pk>/delete', BlogDeleteView.as_view(), name='blogger-delete'), 
--- blog/blogger/views.py
+++ blog/blogger/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView,  DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from .models import Blog
 
 
@@ -17,7 +18,19 @@ class BlogListView(ListView):
     model = Blog
     template_name = 'blogger/home.html'
     context_object_name = 'posts'
-    paginate_by = 3
+    paginate_by = 5
+
+
+class UserBlogListView(ListView):   #this get the auther's total blog posted
+    model = Blog
+    template_name = 'blogger/user_blogs.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+    
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Blog.objects.filter(author=user)
+
 
 class BlogDetailView(DetailView):
     model = Blog
